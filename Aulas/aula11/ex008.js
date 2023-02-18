6cc8a27f7488f6ae6f8f31b177b59380a35a4a7a
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-18 08:59:38 -0300

    Update ex008.js

--- Aulas/aula11/ex008.js
+++ Aulas/aula11/ex008.js
@@ -1,9 +1,9 @@
 
 // console.log("O console funciona!!!")
-var vel = 12.5
+var vel = 61.5
 console.log(`A velocidade de seu carro é ${vel}km/h `)
 // console.log(`Você ultrapassou o limite de velocidade: Multado!!!`)
-if(vel > 60){
+if(vel > 60){ // Condição simples
     console.log(`Você ultrapassou o limite de velocidade: Multado!!!`)
 }
 console.log(`Dirija sempre usando o cinto de segurança`)
