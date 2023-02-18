a3f54f52215b1a431590620396aed28bce2d0661
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-18 09:04:34 -0300

    Condição composta

---
+++ Aulas/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Nasceu em ${pais}`)
+
+if (pais == 'Brasil'){ // Condição composta
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
+}
