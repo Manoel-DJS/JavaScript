4cf91adcd7ebe78c451f3a11781818f48504030e
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-24 17:24:20 -0300

    Update script.js

--- Aulas/aula14ex/ex16/script.js
+++ Aulas/aula14ex/ex16/script.js
@@ -1,13 +1,24 @@
 function clicar(){
     var tini = window.document.getElementById('txtini')
     var tpas = window.document.getElementById('txtpas')
-    var tfim = window.document.querySelector('input#txtini')
+    var tfim = window.document.querySelector('input#txtfim')
     var res = window.document.getElementById('res')
     let i = Number(tini.value)
     let p = Number(tpas.value)
     let f = Number(tfim.value)
+    res.innerHTML += `<p>Valor do I: ${i}</p>`
+    res.innerHTML += `<p>Valor do P: ${p}</p>`
+    res.innerHTML += `<p>Valor do F: ${f}</p>`
 
-    for(var c = i; c <= f; c += p){
-        res.innerHTML += `${c}`
+    if(i < f){
+        for(var c = i ; c <= f ; c += p){
+            res.innerHTML +=` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1} `
+    }else{
+        for(var c = i; c >= f ; c -= p){
+            res.innerHTML +=` ${c} \u{1F449}`
+        }
+        res.innerHTML += ` \u{1F3C1} ` 
     }
 }
