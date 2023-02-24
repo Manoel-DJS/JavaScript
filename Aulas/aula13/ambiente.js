926622b6f1706bacbdef7ee00c4aa2387fc0fbfd
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-23 21:25:24 -0300

    Repetições (Parte 1)

---
+++ Aulas/aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do  {
+    console.log(`Contador está no número: ${c}`)
+    c++
+} while (c <= 5)
+
+/** 
+    while: Teste Lógico no início
+    "Faz o teste, se for true, refaz o bloco(bloco == entre chaves {})"
+
+var x = 1
+while(x <= 5){
+    console.log(`Número do Contador: ${x}`)
+    x+=1
+}
+**/
+
+/**
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+ **/
