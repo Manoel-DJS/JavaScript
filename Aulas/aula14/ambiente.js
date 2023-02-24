94d062710cb8daea61a34dedc82845e46d90bb27
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-24 09:46:02 -0300

    Repetições (Parte 2)
    repetição for

---
+++ Aulas/aula14/ambiente.js
@@ -0,0 +1,9 @@
+for(var c = 1; c <= 5; c++){
+    console.log(`Contador For: ${c}`)
+} // Quando for finito
+
+var c = 1
+while(c <= 5){
+    console.log(`Contador While: ${c}`)
+    c++
+} // Quando for infinito
