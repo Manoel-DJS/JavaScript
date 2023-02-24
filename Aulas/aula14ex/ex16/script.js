560a7681ca94d3a2ddeda2e3118a899206f4dabf
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-02-24 10:35:00 -0300

    Exercícios JavaScript Repetições

---
+++ Aulas/aula14ex/ex16/script.js
@@ -0,0 +1,14 @@
+function clicar(){
+    var tini = window.document.getElementById('txtini')
+    var tpas = window.document.getElementById('txtpas')
+    var tfim = window.document.querySelector('input#txtini')
+    var res = window.document.querySelector('div#res')
+    let ini = Number(tini.value)
+    let pas = Number(tpas.value)
+    let fim = Number(tfim.value)
+    
+    for( ini; ini <= fim; pas+=pas){
+        res.innerHTML +=
+    }
+
+}
