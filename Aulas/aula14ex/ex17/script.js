cb442986f07035f9025027a80d05fbd06eee59a6
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-01 11:23:07 -0300

    Att Ex tabuada
    Select incluído

--- Aulas/aula14ex/ex17/script.js
+++ Aulas/aula14ex/ex17/script.js
@@ -1,10 +1,27 @@
 function clicar() {
     let num = window.document.getElementById('txtn')
-    let res = window.document.getElementById('res')
-    var n = Number(num.value)
-    res.innerHTML = ''
-    res.innerHTML = `<p>Tabuada de ${n}</p>`
-    for( var c = 0 ; c <= 10 ; c++){
-        res.innerHTML += `<p> ${n} x ${c} = ${n*c} </p>`
-    }
+    let tab = window.document.querySelector('select#seltab')
+
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um número: ')
+    } else{
+        var n = Number(num.value)
+        tab.innerHTML = ''
+
+        for( var c = 0 ; c <= 10 ; c++){
+            let item = window.document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+
+
+        }
 }
+}
+
+/*
+        res.innerHTML += `<p>Tabuada de ${n}</p>`
+    let res = window.document.getElementById('res')
+    res.innerHTML = ''
+    res.innerHTML += `<p> ${n} x ${c} = ${n*c} </p>`
+
+*/
