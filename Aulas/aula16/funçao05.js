b076ce8e39e7bc6bb4f3632ba969bc3feff003f3
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-03 10:11:18 -0300

    fat e recursão
    Lógica do Fatorial
    e
    Função Recursiva

---
+++ Aulas/aula16/funçao05.js
@@ -0,0 +1,20 @@
+// Função Recursiva
+
+/*
+
+5! = 5 x 4 x 3 x 2 x 1
+5! = 5 x 4!
+
+n! = n x (n - 1)
+1! = 1
+*/
+
+function fatorial(n){
+    if(n == 1 ){
+        return 1
+    }else {
+        return n * fatorial(n-1)
+    }
+}
+var res = fatorial(5)
+console.log(`O valor do fatorial de 5 é ${res}`)
