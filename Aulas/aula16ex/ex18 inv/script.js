3d4a3e3b663be41a84eaf6f0a909ee6f5ae5f29f
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-04 18:05:44 -0300

    ex 18 pronto

--- Aulas/aula16ex/ex18 inv/script.js
+++ Aulas/aula16ex/ex18 inv/script.js
@@ -21,9 +21,50 @@ function inLista(n,l){
 
 function adicionar(){
     if (isNumero(numero.value) && !inLista(numero.value, vetor)){
-        window.alert('Tudo nos conformes.')
+        vetor.push(Number(numero.value))
+        let item = window.document.createElement('option')
+        item.text = `Valor ${Number(numero.value)} foi adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else{
         window.alert('Valor encontrado ou inválido.')
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function finalizar(){
+    if(vetor.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+        numero.focus()
+    } else{
+        let tot = vetor.length
+        let soma = 0
+        let maior = vetor[0]
+        let menor = vetor[0]
+        let media = 0
+
+        for(var p = 0; p <= tot; p++){
+            if(vetor[p] > maior){
+                maior = vetor[p]
+            }
+            if(vetor[p] < menor){
+                menor = vetor[p]
+            }
+            
+        }
+        for(let pos in vetor){ // Se usar o VETOR, CORRA PRA ESSE
+            soma += vetor[pos]
+        }
+
+        media = soma /tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>A soma de todos os valores resulta: ${soma}</p>`
+        res.innerHTML += `<p>O maior número é: ${maior}</p>`
+        res.innerHTML += `<p>O menor número é: ${menor}</p>`
+        res.innerHTML += `<p>A média dos valores é: ${media} </p>`
+
     }
 }
 /*
