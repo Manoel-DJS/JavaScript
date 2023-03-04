d283ec61671627c48f9daf3acea4ac05b2c2170b
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-04 10:07:35 -0300

    Ex 18

---
+++ Aulas/aula16ex/ex18/script.js
@@ -0,0 +1,5 @@
+function adicionar(){
+    let numero = window.document.getElementById('txtnum')
+    let res = window.document.querySelector('div#res')
+    if (numero.value == 0 || numero.value > 100)
+}
