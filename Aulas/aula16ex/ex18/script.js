7c4ca6356b00156fc2f5dcd54f08942b4c80bffe
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-04 10:44:28 -0300

    função adicionar

--- Aulas/aula16ex/ex18/script.js
+++ Aulas/aula16ex/ex18/script.js
@@ -1,5 +1,21 @@
+var vetor = []
+
 function adicionar(){
     let numero = window.document.getElementById('txtnum')
     let res = window.document.querySelector('div#res')
-    if (numero.value == 0 || numero.value > 100)
+    let tab = window.document.getElementById('seltab')
+    
+
+    if (numero.value <= 0 || numero.value > 100){
+        window.alert('Digite um número entre 1 a 100')
+    } else{
+        var n = parseInt(numero.value)
+        vetor.push(n)
+
+        var item = window.document.createElement('Option')
+        item.text = `O valor ${n} foi adicionado`
+        tab.appendChild(item)
+        
+    }
 }
+
