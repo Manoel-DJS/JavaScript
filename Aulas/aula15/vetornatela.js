5cf71cc6b778d9a90cbce63d98968cf8aaf9379f
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-04 11:27:47 -0300

    Programa com erro

--- Aulas/aula15/vetornatela.js
+++ Aulas/aula15/vetornatela.js
@@ -10,6 +10,9 @@ for(var p = 0 ; p < valores.length ; p++){
 
 let valores = [8, 1, 7, 4, 2, 9]
 console.log(valores)
+media = 0
 for(var p in valores){
     console.log(`O valor na posição ${p} é ${valores[p]}`)
+    media += valores[p]
 }
+console.log(media)
--- Aulas/aula16ex/ex18/script.js
+++ Aulas/aula16ex/ex18/script.js
@@ -10,12 +10,26 @@ function adicionar(){
         window.alert('Digite um número entre 1 a 100')
     } else{
         var n = parseInt(numero.value)
-        vetor.push(n)
-
-        var item = window.document.createElement('Option')
-        item.text = `O valor ${n} foi adicionado`
-        tab.appendChild(item)
+        var test = n
+            vetor.push(n)
+            var item = window.document.createElement('Option')
+            item.text = `O valor ${n} foi adicionado`
+            tab.appendChild(item)
+        }
         
     }
-}
 
+
+function finalizar(){
+    let res = window.document.querySelector('div#res')
+    let quant = vetor.length
+    let soma = 0
+    let menor = 0
+    let maior = 0
+
+    for(var c = 0;c <= 3; c++){
+        var media = media + vetor[c]
+    }       
+
+    res.innerHTML += `Vetor = ${vetor} e quant ${media}`
+}
