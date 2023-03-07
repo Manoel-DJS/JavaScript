2a12e62c170f7ecfaef4f337c5dffb6aedd80241
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-07 10:52:43 -0300

    Update ex06.js
    2 funções em conjunto
    Programa sobre a hora

--- Exercício/ExerciciosFuncoes/ex06.js
+++ Exercício/ExerciciosFuncoes/ex06.js
@@ -5,19 +5,26 @@ Faça um programa que converta da notação de 24 horas para a notação de 12 h
 */
 
  // Testes e mais testes
-
- function notação(h){
+ // Usando 2 funções
+ // necessário diminuir a quantidade de linhas de códigos
+ 
+ function notação(h,m){
     S = ''
     if (h < 0 || h > 24){
         return h.toString() + 'h Hora Inválida! tem que ser entre 0 e 24' 
     } else{
-        PH = `${AMPM(h)}`
-        if(h < 12){
-            return h.toString() + ' ' + PH
+        if(m < 0 || m > 60){
+            console.log('Temos um erro nos minutos (Digite um valor entre 0 e 60)')
         } else{
-            var tarde = h - 12
-            return tarde.toString() + ' ' + PH
+            PH = `${AMPM(h)}`
+            if(h < 12){
+                return h.toString() + `:${m}` + ' ' + PH
+            } else{
+                var tarde = h - 12
+                return tarde.toString() + `:${m}` + ' ' + PH
+            }
         }
+
     }
 }
 
@@ -30,5 +37,5 @@ function AMPM(P){
 
 }
 
-hora = notação(14)
+hora = notação(6,35)
 console.log(hora)
