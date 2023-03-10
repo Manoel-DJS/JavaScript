eebd305849c1b25e1058296f7079b7f854ab2685
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-09 21:50:58 -0300

    Create ex08.js

---
+++ Exercício/ExerciciosFuncoes/ex08.js
@@ -0,0 +1,13 @@
+/*
+
+(8)
+Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+*/
+function contar(n){
+    S = n.toString()
+    return S.length // Para contar os dígitos tem q tá na formatação String
+
+}
+chamada = contar(999)
+console.log(chamada)
