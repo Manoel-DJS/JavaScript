23a71164ae68fd256b6104eec54d3acfb3f39fb7
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-10 09:34:58 -0300

    Reverso do Número ex 09
    Com explicação

--- Exercício/ExerciciosFuncoes/ex09.js
+++ Exercício/ExerciciosFuncoes/ex09.js
@@ -3,12 +3,19 @@
 Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
 
 */
-function reverter(n){
-    S = n.toString()
-    S.sort()
-    return S
+
+function reverter(n){ // 2 o parametro
+    let numero = n.toString() // passando n para string
+    let saida = '' // criando a variável de saida
+
+    for(var i = numero.length -1 ; i >= 0; i--){ // numero.length -1 é a contagem de caracteres -1
+        saida += numero[i]
+    }
+    
+    return saida
     }
 
+chamadaf = reverter(123) // Primeiro a chamada
+console.log(chamadaf)
 
-
-chamadaf = reverter(259) 
+// numero.length daria 3, mas o [i] tem que começar de 0. Por isso o -1 ^^, ai a repetição vai começar do ultimo caractere e vai até o primeiro 2,1,0... Fosse um número maior seria a mesma coisa.
