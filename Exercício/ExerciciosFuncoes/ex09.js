28d685434daac232a9961016d8d2d2823780dae1
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-03-09 22:05:49 -0300

    Create ex09.js

---
+++ Exercício/ExerciciosFuncoes/ex09.js
@@ -0,0 +1,14 @@
+/*
+
+Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
+
+*/
+function reverter(n){
+    S = n.toString()
+    S.sort()
+    return S
+    }
+
+
+
+chamadaf = reverter(259) 
